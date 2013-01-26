d40d4f62c887ab76642f0bc307773755637d7307
romainberger <signup@romainberger.com>
2013-01-26 20:09:45 +0100

    Removed update stuff

--- all/index.js
+++ all/index.js
@@ -17,18 +17,6 @@ function Generator() {
 
 util.inherits(Generator, yeoman.generators.NamedBase);
 
-// Upgrade
-/*
-Generator.prototype.update = function update() {
-  var cb = this.async();
-
-  var packagePath = path.join(__dirname, '../package.json');
-  updater.getUpdate({ localPackageUrl: packagePath }, function() {
-    cb();
-  });
-}
-*/
-
 // get the latest stable version of Wordpress
 Generator.prototype.getVersion = function getVersion() {
   var cb = this.async(),
