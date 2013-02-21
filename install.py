1feb6c956b64d44cf957d4b46d881b6b069afb2c
romainberger <romain@romainberger.com>
2013-02-21 16:19:13 +0100

    Edited install script to make it more generic

--- install.py
+++ install.py
@@ -1,26 +1,32 @@
+"""
+    Simple script to globally install a Yeoman generator
+
+    If you want to use this script for your generator just
+    change the name in `generator_name` and it should be good
+"""
+
 import os
 import shutil
 
-# Script to install the generator.
-# I am a novice in python so it's certainly not perfect. If you know a way to improve it, feel free to fork it
+generator_name = 'yeoman-wordpress'
 
-generatorPath = '/usr/local/lib/node_modules/yeoman/node_modules/yeoman-generators/lib/generators'
-currentDir = os.path.dirname(os.path.abspath(__file__))
-yeomanWordpressPath = os.path.join(generatorPath, 'yeoman-wordpress')
+yeoman_generator_path = '/usr/local/lib/node_modules/yeoman/node_modules/yeoman-generators/lib/generators'
+current_dir = os.path.dirname(os.path.abspath(__file__))
+generator_path = os.path.join(yeoman_generator_path, generator_name)
 
 # Check if Yeoman is installed
-if not os.path.isdir(generatorPath):
+if not os.path.isdir(yeoman_generator_path):
 	print 'It seems that Yeoman is not installed.'
 	print 'If you have trouble installing the generator please refer to the install instructions in the Readme.'
 	os.exit()
 
-# if the generator is already installed we remove it to avoid errors
-if os.path.isdir(yeomanWordpressPath):
-	shutil.rmtree(yeomanWordpressPath)
+# If the generator is already installed we remove it to avoid errors
+if os.path.isdir(generator_path):
+	shutil.rmtree(generator_path)
 try:
-	shutil.copytree(currentDir, yeomanWordpressPath)
+	shutil.copytree(current_dir, generator_path)
 	print '\033[32mGenerator installed successfully.\033[37m\n'
-except :
+except:
 	print 'Oops, something went wrong, sorry'
 	print 'If you have trouble installing the generator please refer to the install install instructions in the Readme.'
 	raise
