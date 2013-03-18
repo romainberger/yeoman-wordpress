76f2f5361f70a36dc3299ec1d14dff5364e89638
romainberger <romain@romainberger.com>
2013-03-18 13:34:54 +0100

    Fixed coffee script compiling error

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function( grunt ) {
     coffee: {
       dist: {
         src: 'app/wp-content/themes/<%= themeName %>/js/*.coffee',
-        dest: 'app/wp-content/themes/<%= themeName %>/js'
+        dest: 'app/wp-content/themes/<%= themeName %>/js/*.js'
       }
     },
 
